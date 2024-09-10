cd47e368108fb8bf85533dfa6a5b1c7a374b4a40
brian200508 <36002167+brian200508@users.noreply.github.com>
2024-09-10 16:04:47 +0200

    Create fix-desktop-links.sh
    Skript for fixing Chromium and VSCode desktop links (add flag --no-sandbox).

---
+++ fix-desktop-links.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Fix desktop links"
+echo "Updating..."
+sudo apt update -y
+
+echo "Fixing VSCode desktop links..."
+sed -i 's@code --new-window \%F@code --no-sandbox --new-window \%F@g' /usr/share/applications/code.desktop
+sed -i 's@code \%F@code --no-sandbox \%F@g' /usr/share/applications/code.desktop
+
+echo "Fixing Chromium desktop links..."
+sed -i 's@chromium \%U@chromium --no-sandbox \%U@g' /usr/share/applications/chromium.desktop
+
+echo "Done."
