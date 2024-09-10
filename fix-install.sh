3e2086d7b977feb636f666964d82280610299768
brian200508 <36002167+brian200508@users.noreply.github.com>
2024-09-10 16:41:26 +0200

    Update fix-install.sh:fix nvm install

--- fix-install.sh
+++ fix-install.sh
@@ -53,7 +53,10 @@ echo "Setting up Node.js 20.x-LTS..."
 sudo apt update -y
 # installs nvm (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-source ~/.bashrc
+#FIXME: . ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 # download and install Node.js (you may need to restart the terminal)
 nvm install 20
 
