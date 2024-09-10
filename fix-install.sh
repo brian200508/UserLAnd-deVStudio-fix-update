2d4ef5638fb70f9171ba74904a41af65ec625384
brian200508 <36002167+brian200508@users.noreply.github.com>
2024-09-10 16:10:22 +0200

    Update fix-install.sh: fix desktop links

--- fix-install.sh
+++ fix-install.sh
@@ -19,8 +19,8 @@ echo "Setting up latest VSCode..."
 #code --no-sandbox 
 sudo apt install -y code # or code-insiders 
 sudo apt update -y
-sed -i 's@code --new-window \%F@code --no-sandbox --new-window \%F@g' /usr/share/applications/code.desktop
-sed -i 's@code \%F@code --no-sandbox \%F@g' /usr/share/applications/code.desktop
+#sed -i 's@code --new-window \%F@code --no-sandbox --new-window \%F@g' /usr/share/applications/code.desktop
+#sed -i 's@code \%F@code --no-sandbox \%F@g' /usr/share/applications/code.desktop
 
 # Install Chromium Browser
 echo "Setting up Chromium Browser..."
@@ -30,7 +30,7 @@ sudo add-apt-repository ppa:xtradeb/apps -y
 sudo apt update -y
 sudo apt install -y chromium
 sudo apt update -y
-sed -i 's@chromium \%U@chromium --no-sandbox \%U@g' /usr/share/applications/chromium.desktop
+#sed -i 's@chromium \%U@chromium --no-sandbox \%U@g' /usr/share/applications/chromium.desktop
 #chromium --no-sandbox 
 
 # Git, Python3 and essentials
@@ -46,7 +46,13 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 # download and install Node.js (you may need to restart the terminal)
 nvm install 20
 
-# Git config
+# fix desktop links
+cp ./fix-desktop-links.sh ~/fix-desktop-links.sh
+chmod +x ./fix-desktop-links.sh
+chmod +x ~/fix-desktop-links.sh
+~/fix-desktop-links.sh
+
+# Summary
 echo "Done."
 echo ""
 echo "Installed versions:"
@@ -60,6 +66,10 @@ echo ""
 echo "Don't forget Your Git config:"
 echo "    git config --global user.name \"Your Name\""
 echo "    git config --global user.email \"your.email-address@domain.com\""
+echo ""
+echo "After Chromium or VSCode update You can fix the desktop application links"
+echo "by running this command:"
+echo "    ~/fix-desktop-links.sh"
 echo ""
 echo "For deVStudio:"
 echo "Close VSCode and restart with:"
