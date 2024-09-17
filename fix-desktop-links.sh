bce70520c509a6df7f232f7291b32734015e109b
brian200508 <36002167+brian200508@users.noreply.github.com>
2024-09-17 17:57:54 +0000

    fix Web Browser desktop links

--- fix-desktop-links.sh
+++ fix-desktop-links.sh
@@ -11,4 +11,7 @@ sed -i 's@code \%F@code --no-sandbox \%F@g' /usr/share/applications/code.desktop
 echo "Fixing Chromium desktop links..."
 sed -i 's@chromium \%U@chromium --no-sandbox \%U@g' /usr/share/applications/chromium.desktop
 
+echo "Fixing Web Browser desktop links..."
+sed -i 's@WebBrowser \%U@WebBrowser --no-sandbox \%U@g' /usr/share/applications/xfce4-web-browser.desktop
+
 echo "Done."
