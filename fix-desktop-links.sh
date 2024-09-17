65d4dd161610cbe03462f6982fbda61b248968d3
brian200508 <36002167+brian200508@users.noreply.github.com>
2024-09-17 18:33:13 +0000

    fix upper lower case

--- fix-desktop-links.sh
+++ fix-desktop-links.sh
@@ -6,12 +6,16 @@ sudo apt update -y
 
 echo "Fixing VSCode desktop links..."
 sed -i 's@code --new-window \%F@code --no-sandbox --new-window \%F@g' /usr/share/applications/code.desktop
+sed -i 's@code --new-window \%f@code --no-sandbox --new-window \%f@g' /usr/share/applications/code.desktop
 sed -i 's@code \%F@code --no-sandbox \%F@g' /usr/share/applications/code.desktop
+sed -i 's@code \%f@code --no-sandbox \%f@g' /usr/share/applications/code.desktop
 
 echo "Fixing Chromium desktop links..."
 sed -i 's@chromium \%U@chromium --no-sandbox \%U@g' /usr/share/applications/chromium.desktop
+sed -i 's@chromium \%u@chromium --no-sandbox \%u@g' /usr/share/applications/chromium.desktop
 
 echo "Fixing Web Browser desktop links..."
 sed -i 's@WebBrowser \%U@WebBrowser --no-sandbox \%U@g' /usr/share/applications/xfce4-web-browser.desktop
+sed -i 's@WebBrowser \%u@WebBrowser --no-sandbox \%u@g' /usr/share/applications/xfce4-web-browser.desktop
 
 echo "Done."
